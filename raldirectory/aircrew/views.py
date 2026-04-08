9d6c8e27af0a27d4b88293bad97b02eae32500f4
Temka0994 <pleskovartem878@gmail.com>
2026-04-08 21:57:33 +0300

    Add aircrew main page.

--- raldirectory/aircrew/views.py
+++ raldirectory/aircrew/views.py
@@ -1,1 +1,14 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render
+
 from .models import Aircrew
+
+
+def aircrew_main(request):
+    aircrew = Aircrew.objects.select_related('rank').order_by('id')
+
+    paginator = Paginator(aircrew, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "aircrew/main.html", {"page_obj": page_obj})
--- raldirectory/raldirectory/urls.py
+++ raldirectory/raldirectory/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from aircraft.views import aircraft_main
+from aircrew.views import aircrew_main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('aircraft/', include("aircraft.urls")),
     path('aircrafts/', aircraft_main, name='aircraft_main'),
     path('aircrew/', include("aircrew.urls")),
+    path('aicrews/', aircrew_main, name='aircrew_main'),
 ]
